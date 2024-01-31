d3fd9ffc9f0c59531f683946a43371bcbbfd6ca8
glasmacherk <glasmacherk@emmanuel.edu>
2024-01-30 22:42:05 -0500

    function for stacked barplot

--- output_data/plots/stacked_bar_plot.R
+++ output_data/plots/stacked_bar_plot.R
@@ -1,38 +1,32 @@
 library(tidyverse)
 library(plotly)
 
-luad_attr$effect_shares
 
-first_sample <- luad_cesa$trinuc_rates %>%
-  filter(Unique_Patient_Identifier == "TCGA-05-4244")
-
-signatures <- read.table("COSMIC_v3.4_SBS_GRCh37.txt", header = TRUE)
-signatures_longer <- signatures %>%
-  pivot_longer(cols = starts_with("SBS"), names_to = "Signatures", values_to = "raw_signature")
-
-signatures_longer %>%
-  filter(Signatures == "SBS1") %>%
-  filter(Values > 0.1) %>%
-  ggplot() +
-  geom_col(aes(x=Type, y=Values))
-
-
-first_example <- luad_cesa@trinucleotide_mutation_weights[["raw_signature_weights"]] %>%
-  filter(Unique_Patient_Identifier == "TCGA-05-4244") %>%
-  pivot_longer(cols = starts_with("SBS"), names_to = "Signatures", values_to = "raw_signature_weights")
-
-highest_sigs <- signatures_longer %>%
-  right_join(first_example, by = "Signatures") %>%
-  mutate(absolute_signature = raw_signature*raw_signature_weights) %>%
-  group_by(Type) %>%
-  summarize(sum = sum(absolute_signature)) %>%
-  filter(sum > 4)
-
-plot_try <- signatures_longer %>%
-  right_join(first_example, by = "Signatures") %>%
-  mutate(absolute_signature = raw_signature*raw_signature_weights) %>%
-  filter(Type %in% highest_sigs$Type) %>%
-  ggplot() +
-  geom_bar(aes(x=Type, y=absolute_signature, fill = Signatures), stat = "identity", position = "stack")
-
-ggplotly(plot_try)
+# dataset_name of the form luad_cesa, lusc_cesa etc.
+tcga_to_bar_plot <- function(dataset_name, sample){
+  
+  dataset <- get(dataset_name)
+  
+  signatures <- read.table("COSMIC_v3.4_SBS_GRCh37.txt", header = TRUE)
+  signatures_longer <- signatures %>%
+    pivot_longer(cols = starts_with("SBS"), names_to = "Signatures", values_to = "raw_signature")
+  
+  sample_example <- dataset@trinucleotide_mutation_weights[["raw_signature_weights"]] %>%
+    filter(Unique_Patient_Identifier == sample) %>%
+    pivot_longer(cols = starts_with("SBS"), names_to = "Signatures", values_to = "raw_signature_weights")
+  
+  sample_highest_sigs <- signatures_longer %>%
+    right_join(sample_example, by = "Signatures") %>%
+    mutate(absolute_signature = raw_signature*raw_signature_weights) %>%
+    group_by(Type) %>%
+    summarize(sum = sum(absolute_signature)) %>%
+    slice_max(sum, n = 10)
+  
+  signatures_longer %>%
+    right_join(sample_example, by = "Signatures") %>%
+    mutate(absolute_signature = raw_signature*raw_signature_weights) %>%
+    filter(Type %in% sample_highest_sigs$Type) %>%
+    ggplot() +
+    geom_bar(aes(x=Type, y=absolute_signature, fill = Signatures), stat = "identity", position = "stack") +
+    labs(x="Number of substitutions", y="Trinucleotide context")
+}
